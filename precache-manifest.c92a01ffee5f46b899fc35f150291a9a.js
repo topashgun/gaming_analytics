12606208f749485e44437b440599eada990e1cc5
topashgun <topashgun@gmail.com>
2020-07-26 17:25:37 +0530

    Updates

---
+++ precache-manifest.c92a01ffee5f46b899fc35f150291a9a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a1af58283dca1873882aed47bc2222f",
+    "url": "/gaming_analytics/index.html"
+  },
+  {
+    "revision": "e3a6279dfa6ad43bae2a",
+    "url": "/gaming_analytics/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "df07fea103bde9fcb70a",
+    "url": "/gaming_analytics/static/css/main.6e69fbc4.chunk.css"
+  },
+  {
+    "revision": "e3a6279dfa6ad43bae2a",
+    "url": "/gaming_analytics/static/js/2.2bef1558.chunk.js"
+  },
+  {
+    "revision": "4ed8bb8d2076a9a209cd6042d676dc1d",
+    "url": "/gaming_analytics/static/js/2.2bef1558.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df07fea103bde9fcb70a",
+    "url": "/gaming_analytics/static/js/main.b1f0b936.chunk.js"
+  },
+  {
+    "revision": "3fd905169a90b1a416bb",
+    "url": "/gaming_analytics/static/js/runtime-main.ccd969e3.js"
+  },
+  {
+    "revision": "79dc85dd3e9b8de74160e3118eefcc09",
+    "url": "/gaming_analytics/static/media/data.79dc85dd.csv"
+  },
+  {
+    "revision": "5ea61021d621bf0360b86c4c202041c9",
+    "url": "/gaming_analytics/static/media/logo.5ea61021.png"
+  }
+]);
